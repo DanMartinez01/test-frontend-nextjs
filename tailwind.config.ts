a432481ca60c669a1c8611e117986f5668b391a4
Daniela <martinezdl_@hotmail.com>
2024-04-19 20:14:47 -0300

    add new colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+// import config from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Mulish', 'sans-serif']
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        "black":"#11142D",
+        "gray":{
+          "100":"#E2E2EA",
+          "200":"#92929D",
+          "500":"#9A9AB0"
+        },
+        "purple":"#5541D7",
+        "white":{
+          "500":"#FFFFFF"
+        },
+        "green":"#42BDA1"
+
+        
+      }
     },
   },
   plugins: [],
